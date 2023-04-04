eb1bd960a6d9d8496ec588d10f55ed2ee03fb228
pkyleZbook <kyle.park@ultradent.com>
2023-04-04 11:33:07 -0600

    deleting cmake-build-debug folder so it can be ignored in future

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pkyle/CLionProjects/ros2Test
-# Build directory: /home/pkyle/CLionProjects/ros2Test/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
